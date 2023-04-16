3cae2bcea1a47cfc8c08a0d9c74d517246fc593c
NikolaDS12 <130931004+NikolaDS12@users.noreply.github.com>
2023-04-16 22:50:07 +0300

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Nikiiii</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f0f0f0;
+      }
+      #header {
+        background-color: #333;
+        color: #fff;
+        padding: 20px;
+        text-align: center;
+      }
+      #content {
+        margin: 20px;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px #999;
+      }
+      h1 {
+        margin-top: 0px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="header">
+      <h1>Nikiiii</h1>
+    </div>
+    <div id="content">
+      <h2>Welcome!</h2>
+      <p>This is a simple website created using HTML and CSS.</p>
+      <h3>Features</h3>
+      <ul>
+        <li>Responsive layout</li>
+        <li>Simple design</li>
+        <li>Easy to customize</li>
+      </ul>
+    </div>
+  </body>
+</html>
